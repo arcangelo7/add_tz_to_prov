316ebdbd1e3a4ee455836dd507f03580aa4de959
arcangelo7 <arcangelomas@gmail.com>
2022-10-25 19:47:54 +0200

    Always convert to UTC given a zone and naive times

--- add_tz_to_prov.py
+++ add_tz_to_prov.py
@@ -14,9 +14,9 @@
 
 
 from csv import DictReader
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timezone
 from io import TextIOWrapper
-from rdflib import ConjunctiveGraph, Literal, URIRef, XSD
+from rdflib import ConjunctiveGraph
 from shutil import make_archive, rmtree
 from tqdm import tqdm
 from typing import List
@@ -24,6 +24,7 @@ from zipfile import ZipFile, ZipExtFile
 import argparse
 import csv
 import os
+import pytz
 import re
 
 
@@ -41,22 +42,22 @@ def write_csv(path:str, datalist:List[dict], fieldnames:list=None) -> None:
             dict_writer.writeheader()
             dict_writer.writerows(datalist)
 
-def add_tz_to_csv(f:ZipExtFile, tz_offset:timedelta, output_path:str) -> None:
+def add_tz_to_csv(f:ZipExtFile, output_path:str, zone:str) -> None:
     data = list(DictReader(TextIOWrapper(f)))
     for row in data:
         if row['created']:
-            row['created'] = datetime.strptime(row['created'], '%Y-%m-%dT%H:%M:%S').replace(tzinfo=timezone(offset=tz_offset)).isoformat(sep='T')
+            row['created'] = get_utc_time_str(row['created'], zone)
         if row['invalidated']:
-            row['invalidated'] = datetime.strptime(row['created'], '%Y-%m-%dT%H:%M:%S').replace(tzinfo=timezone(offset=tz_offset)).isoformat(sep='T')
+            row['invalidated'] = get_utc_time_str(row['invalidated'], zone)
     write_csv(output_path, data)
 
-def add_tz_to_nquads(f:ZipExtFile, tz_offset:timedelta, output_path:str) -> None:
+def add_tz_to_nquads(f:ZipExtFile, output_path:str, zone:str) -> None:
     new_lines = []
     for line in f:
         line_str = line.decode('utf8')
         if 'http://www.w3.org/ns/prov#generatedAtTime' in line_str or 'http://www.w3.org/ns/prov#invalidatedAtTime' in line_str:
             time_str = re.search('\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}', line_str).group(0)
-            time_tz = datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S').replace(tzinfo=timezone(offset=tz_offset)).isoformat(sep='T')
+            time_tz = get_utc_time_str(time_str, zone)
             new_lines.append(line_str.replace(time_str, time_tz))
         else:
             new_lines.append(line_str)        
@@ -64,7 +65,7 @@ def add_tz_to_nquads(f:ZipExtFile, tz_offset:timedelta, output_path:str) -> None
         for line in new_lines:
             f.write(line)
 
-def add_tz_to_prov(src:str, dst:str, file_format:str, tz_offset:timedelta) -> ConjunctiveGraph:
+def add_tz_to_prov(src:str, dst:str, file_format:str, zone:str) -> ConjunctiveGraph:
     filenames = os.listdir(src)
     pbar = tqdm(total=len(filenames))
     os.makedirs(dst, exist_ok=True)
@@ -77,18 +78,30 @@ def add_tz_to_prov(src:str, dst:str, file_format:str, tz_offset:timedelta) -> Co
             for archived_file in archived_files:
                 with archive.open(archived_file) as f:
                     output_path = os.path.join(dst_folder, archived_file.replace('_', '-').replace(':', '_'))
-                    eval(f'add_tz_to_{file_format}(f, tz_offset, output_path)')
+                    eval(f'add_tz_to_{file_format}(f, output_path, zone)')
         make_archive(base_name=dst_folder, format='zip', root_dir=dst_folder)
         rmtree(dst_folder)
         pbar.update()
     pbar.close()
 
+def get_utc_time_str(naive_time_str:str, zone:str) -> str:
+    """
+    Given a naive time string in the format '%Y-%m-%dT%H:%M:%S', returns a time string in the same format localised with the UTC time zone.
+
+    :param naive_time_str: a naive time string in the format '%Y-%m-%dT%H:%M:%S'
+    :type string: str
+    :return: str -- a time string in the format '%Y-%m-%dT%H:%M:%S' localised with the UTC time zone
+    """
+    local = pytz.timezone(zone)
+    naive = datetime.strptime(naive_time_str, "%Y-%m-%dT%H:%M:%S")
+    local_dt = local.localize(naive)
+    return local_dt.astimezone(timezone.utc).isoformat(sep="T")
 
 if __name__ == '__main__': # pragma: no cover
-    parser = argparse.ArgumentParser(description='Add timezone information to provenance zipped files containing naive time indications')
+    parser = argparse.ArgumentParser(description='Given an OpenCitations provenance dump containing naive time strings in the format "%Y-%m-%dT%H:%M:%S", localise such times with the UTC time zone')
     parser.add_argument('-s', '--src', dest='src', required=True, help='The folder containing the provenance archives')
     parser.add_argument('-d', '--dst', dest='dst', required=True, help='The folder where new archives will be saved')
     parser.add_argument('-f', '--file_format', dest='file_format', required=True, help='The format of files contained in archives')
-    parser.add_argument('-t', '--tz_offset', dest='tz_offset', required=True, type=int, help='An integer representing the UTC offset to be added')
+    parser.add_argument('-z', '--zone', dest='zone', required=True, help='A timezone listed in pytz.all_timezones, such as "Europe/Rome"')
     args = parser.parse_args()
-    add_tz_to_prov(src=args.src, dst=args.dst, file_format=args.file_format, tz_offset=timedelta(hours=args.tz_offset))
+    add_tz_to_prov(src=args.src, dst=args.dst, file_format=args.file_format, zone=args.zone)
